f536a18e1f4e29ec04b29090b09762e34fb1e3fe
David Tyler <davet.code@gmail.com>
2013-05-27 11:48:29 +0100

    Finalised adding stat values and added some commenting on each action function

--- src/Tyler/TopTrumpsBundle/Controller/JSONCardController.php
+++ src/Tyler/TopTrumpsBundle/Controller/JSONCardController.php
@@ -9,6 +9,14 @@ use Tyler\TopTrumpsBundle\Entity\StatValue;
 
 class JSONCardController extends AbstractDbController
 {
+    /**
+     * Action to retrieve the card information for a given card/deck combo.
+     *
+     * @param $deckId - The card must be part of this deck or a 404 response
+     * is issued.
+     * @param $cardId - The card to retrieve.
+     * @return Response - The full json blob describing this card.
+     */
     public function getAction($deckId, $cardId)
     {
         $card = $this->checkCardId($deckId, $cardId);
@@ -18,6 +26,14 @@ class JSONCardController extends AbstractDbController
         return new Response($serializer->serialize($card, 'json'), 200);
     }
 
+    /**
+     * Given a card and a json blob this action updates the cards values
+     * including stat values and the image.
+     *
+     * @param int $deckId - The card is checked as being part of this deck.
+     * @param int $cardId - The card to update.
+     * @return Response - The full card with the new information.
+     */
     public function updateAction($deckId, $cardId)
     {
         $em = $this->getDoctrine()->getManager();
@@ -27,6 +43,22 @@ class JSONCardController extends AbstractDbController
         $card->setName($request->request->get('name'));
         $card->setDescription($request->request->get('description'));
 
+        /*
+         * Iterate over the existing stat values and update the values. Note
+         * that all stat values must exist on card creation so we do not have
+         * to either remove or add a stat value to the database.
+         */
+        foreach ($card->getStatValues() as $statValue) {
+            foreach ($request->request->get('stat_values') as $statValueJson) {
+                if ($statValueJson["id"] === $statValue->getId()) {
+                    /*
+                     * Set the current value even if it's the same.
+                     */
+                    $statValue->setValue($statValueJson["value"]);
+                }
+            }
+        }
+
         $em->flush();
 
         $serializer = $this->container->get('serializer');
@@ -34,6 +66,15 @@ class JSONCardController extends AbstractDbController
         return new Response($serializer->serialize($card, 'json'), 200);
     }
 
+    /**
+     * Action to remove a card from a deck. This will cascade down and remove
+     * all dependent data as well.
+     *
+     * @param $deckId - The card must be part of this deck or a 404 will be
+     * returned.
+     * @param $cardId - The card to remove.
+     * @return Response - The card that was removed.
+     */
     public function removeAction($deckId, $cardId)
     {
         $em = $this->getDoctrine()->getManager();
@@ -47,6 +88,13 @@ class JSONCardController extends AbstractDbController
         return new Response($serializer->serialize($card, 'json'), 200);
     }
 
+    /**
+     * Action function used to create a card from a json blob.
+     *
+     * @param int $deckId - Forces the card to be part of this deck.
+     * @return Response - The card object is returned including the newly
+     * created card id.
+     */
     public function createAction($deckId)
     {
         $request = $this->getRequest();
@@ -58,8 +106,8 @@ class JSONCardController extends AbstractDbController
         $card->setDescription($request->request->get('description'));
         $card->setDeck($deck);
 
-        foreach ($request->request->get('stat_values') as $statValueJson) {
-            $stat = $this->checkStatId($deckId, $statValueJson->get("id"));
+        foreach ($request->request->get('stat_values') as $statValueArray) {
+            $stat = $this->checkStatId($deckId, $statValueArray["id"]);
             $statValue = new StatValue();
             $statValue->setCard($card);
             $statValue->setStat($stat);
@@ -70,8 +118,9 @@ class JSONCardController extends AbstractDbController
              *
              * TODO : Enforce in database.
              */
-            $value = $statValueJson->get("value");
+            $value = $statValueArray["value"];
             $statValue->setValue(min(max($stat->getMin(), $value), $stat->getMax()));
+            $em->persist($statValue);
 
             $card->addStatValue($statValue);
         }
